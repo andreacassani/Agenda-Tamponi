714c2ac8d4232088c70fcbacfaead9cc0408de82
Andrea Cassani <32062066+andreacassani@users.noreply.github.com>
2020-11-26 01:33:53 +0100

    Update agendatamponi.meta.js
    Fixed updateURL and downloadURL

--- agendatamponi.meta.js
+++ agendatamponi.meta.js
@@ -5,8 +5,8 @@
 // @description  Miglioramenti per prenotazione di tamponi su progetto SOLE
 // @author       Andrea Cassani
 // @icon         https://i.ibb.co/88kwYf3/icon128.png
-// @updateURL    https://andreacassani.com/static/files/agendatamponi/agendatamponi.meta.js
-// @downloadURL  https://andreacassani.com/static/files/agendatamponi/agendatamponi.user.js
+// @updateURL    https://raw.githubusercontent.com/andreacassani/Agenda-Tamponi/main/agendatamponi.meta.js
+// @downloadURL  https://raw.githubusercontent.com/andreacassani/Agenda-Tamponi/main/agendatamponi.user.js
 // @match        https://ws.bolognaausl.progetto-sole.it/soleweb/?wicket:interface*
 // @match        https://www.progetto-sole.it/riservata*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
